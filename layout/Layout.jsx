eadd9780fdd564dd5eb477124ca69987fec9911d
Aymen Fezai <fezaiaymen1994@gmail.com>
2021-09-22 15:25:36 +0100

    FIX

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -1,6 +1,8 @@
-import BackgroundAnimated from "../components/shared/BackgroundAnimated";
+import dynamic from 'next/dynamic'
 import Footer from "./Footer";
 import Nav from "./Nav";
+
+const BackgroundAnimated = dynamic(() => import("../components/shared/BackgroundAnimated"), { ssr: false })
 
 const Layout = ({ children }) => {
     return (
--- next.config.js
+++ next.config.js
@@ -3,7 +3,4 @@ module.exports = {
   images: {
     domains: ['*'],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
