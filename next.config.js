97e3a4335967622ab0f0c53f52956e6293678b06
Aymen Fezai <fezaiaymen1994@gmail.com>
2021-11-18 16:33:40 +0100

    FIX: Album

--- next.config.js
+++ next.config.js
@@ -5,12 +5,11 @@ const localeSubpaths = {}
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['website-api.noxksa.com'],
+    domains: ['website-api.noxksa.com', '35.193.29.67'],
   },
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
   },
 }
-
 
--- pages/albums/[id].js
+++ pages/albums/[id].js
@@ -12,74 +12,75 @@ import Iframe from 'react-iframe'
 
 const Album = (props) => {
     const router = useRouter();
-    const [img ,setImg]=useState([])
-    const [video ,setVideo]=useState([])
+    const [img, setImg] = useState([])
+    const [video, setVideo] = useState([])
 
-    const [title,setTitle]=useState("")
-    const [description,setDescription]=useState("")
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
 
-    const {data}=props.data.data
-    const links=data.links
-    const images=[]
-    const videos=[]
+    const { data } = props.data.data
+    const links = data.links
+    const images = []
+    const videos = []
     const [isOpen, setIsOpen] = useState(false);
 
 
     useEffect(() => {
-        for (let i=0;i<links.length;i++){
-            const link=`${SERVER_URL}/uploads/${links[i].link}`
-            if(links[i].link.startsWith('album')){
-                images.push({thumbnail:link,src:link ,thumbnailWidth: 320,
-                    thumbnailHeight: 212})
+        for (let i = 0; i < links.length; i++) {
+            const link = `${SERVER_URL}/uploads/${links[i].link}`
+            if (links[i].link.startsWith('album')) {
+                images.push({
+                    thumbnail: link, src: link, thumbnailWidth: 320,
+                    thumbnailHeight: 212
+                })
             }
             else {
-               videos.push(links[i].link)
+                videos.push(links[i].link)
 
             }
 
-               
+
         }
         setVideo(videos)
         setImg(images)
         setTitle(data.title)
         setDescription(data.description)
-        }, []);
-  
+    }, []);
+
 
 
     return (
-        
+
         <>
-      
-        <Layout navWithBackground>
-       
-            <section >
-          
-                <div className="container" style={{ marginTop: 60 }} >
-                  <h1>{title.en}</h1>
-                  <p style={{marginTop:20}}>{description.en}   </p>  
-                   
-                  <div className="album" style={{marginTop:20}}>
-                    <Gallery images={img} backdropClosesModal={true}  margin={5}  onClickThumbnail={() => setIsOpen(true)}/>
-                    {video.map((v,i) => (
-                         <Iframe url={v}
-                            key={i}
-                            width="220px"
-                            height="220px"
-                            id="myId"
-                            />
-                                                
-                        ))}             
 
-                  </div>
-                  {console.log(isOpen)}
+            <Layout navWithBackground>
 
-                </div>
-        
-            </section>
+                <section style={{ position: 'relative', zIndex: 98 }} >
 
-        </Layout>
-      
+                    <div className="container" style={{ marginTop: 60 }} >
+                        <h1>{title.en}</h1>
+                        <p style={{ marginTop: 20 }}>{description.en}   </p>
+
+                        <div className="album" style={{ marginTop: 20 }}>
+                            <Gallery images={img} backdropClosesModal={true} margin={5} onClickThumbnail={() => setIsOpen(true)} />
+                            {video.map((v, i) => (
+                                <Iframe url={v}
+                                    key={i}
+                                    width="220px"
+                                    height="220px"
+                                    id="myId"
+                                />
+                            ))}
+
+                        </div>
+                        {console.log(isOpen)}
+
+                    </div>
+
+                </section>
+
+            </Layout>
+
             <style jsx>{`
                 section {
                     padding-top: 160px;
@@ -102,11 +103,11 @@ const Album = (props) => {
 
 export async function getServerSideProps({ query }) {
     const res = await fetch(`${API_URL}/albums/${query.id}`)
-  const data = await res.json()
+    const data = await res.json()
 
     return {
         props: {
-            data: {data}
+            data: { data }
         },
     };
 }
