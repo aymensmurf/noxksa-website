73284fc7e6f370aaeee91a9c4e56d62ad9704428
Aymen Fezai <fezaiaymen1994@gmail.com>
2022-04-11 11:54:33 +0100

    ADD: GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,33 @@
-import Head from 'next/head'
-import 'react-toastify/dist/ReactToastify.css'
-import '../styles/globals.css'
-import { appWithTranslation } from '../i18n'
-import App from 'next/app'
-
+import Head from "next/head";
+import Script from "next/script";
+import App from "next/app";
+import "react-toastify/dist/ReactToastify.css";
+import "../styles/globals.css";
+import { appWithTranslation } from "../i18n";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Nox Entertainment – شركة كثيب التجارية – خدمات ترفيهية وفعاليات إبداعية</title>
-      </Head>
-      <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Nox Entertainment – شركة كثيب التجارية – خدمات ترفيهية وفعاليات إبداعية</title>
+			</Head>
 
-    </>
-  )
+			<Component {...pageProps} />
+
+			<Script src='https://www.googletagmanager.com/gtag/js?id=UA-225237314-1'></Script>
+			<Script>
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-225237314-1');
+        `}
+			</Script>
+		</>
+	);
 }
 
-MyApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) })
+MyApp.getInitialProps = async (appContext) => ({ ...(await App.getInitialProps(appContext)) });
 
 export default appWithTranslation(MyApp);
